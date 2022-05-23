a3a39d221e07d1979f50a4e07a56f79ed385c591
Nathan McGinn <2986379+nmcginn@users.noreply.github.com>
2022-05-23 10:57:03 -0500

    test env

--- autoformat.sh
+++ autoformat.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+env # let's see what's available
+
+
 dotnet format
+
+set +e
+git diff-index --quiet HEAD
+STATUS=$?
+set -e
+
+if [ $STATUS -eq 0 ]; then
+    echo "No changes detected, your .NET project is already formatted correctly"
+    exit 0
+fi
+
+# echo "TODO: git branch, make a pull request, comment on existing pull request"
